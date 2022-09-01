12f8473cf0502e90cedd8007ecd040755f5f8b8e
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2022-09-01 20:36:01 +0530

    Create sample.py
    '''While purchasing certain items, a discount of 10% is offered
    if the price purchased is more than 100.
    If quantity and price per item
    are input through the keyboard,
    write a program to calculate the total expenses Using simple if statement.

---
+++ sample.py
@@ -0,0 +1,10 @@
+p=int(input("Enter the price: "))
+q=int(input("Enter the quantity: "))
+t=float(p*q)
+if(t>100):
+ t=t-t*0.1
+ print("Total amount to be paid:",t)
+else:
+ print("Total amount to be paid:",t)
+ 
+ 
